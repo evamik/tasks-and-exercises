677c666260f6ab02d9c7b93141ca822037c523b6
Evaldas Mikalauskas <mikalauskas.e@gmail.com>
2021-03-28 18:58:09 +0300

    Finish fuzzy logic lab code

--- Kaunas University of Technology/3rd course/Introduction to Artificial Intelligence/Fuzzy/main.py
+++ Kaunas University of Technology/3rd course/Introduction to Artificial Intelligence/Fuzzy/main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from pathlib import Path
 from dataclasses import dataclass
 from matplotlib import pyplot as plt
+import numpy as np
 
 
 def read_file(filename) -> pd.DataFrame:
@@ -29,13 +30,13 @@ class Trapezoid:
 class FuzzyVar:
     def __init__(self, name):
         self.name = name
-        self.trapezoids = []  # type: List[Trapezoid]
+        self.trapezoids = dict()  # type: dict[str, Trapezoid]
 
-    def add_fuzzy(self, trapezoid):
-        self.trapezoids.append(trapezoid)
+    def add_fuzzy(self, trapezoid: Trapezoid):
+        self.trapezoids[trapezoid.name] = trapezoid
 
     def plot(self):
-        for t in self.trapezoids:
+        for t in self.trapezoids.values():
             plt.text(t.b, 1, t.name, size=15)
             plt.plot(t.to_array(), [0, 1, 1, 0])
         plt.xlim(left=0, right=1)
@@ -56,9 +57,12 @@ class RulesService:
     def get_output(self, x, y, z):
         outputs = []
         for rule in self.rules:
-            outputs.append({rule.value(x, y, z),
-                            rule.trapezoid.name})
-        return outputs
+            outputs.append({"value": rule.value(x, y, z),
+                            "name": rule.trapezoid.name})
+
+        df = pd.DataFrame(outputs).groupby("name")['value'].max()
+
+        return df
 
 
 def f(x, t):
@@ -114,48 +118,94 @@ if __name__ == "__main__":
     # output.plot()
 
     rules.append(Rule(lambda x, y, z:
-                      min(1 - f(x, myAccuracy.trapezoids[0]), f(
-                          y, enemyAccuracy.trapezoids[0])), output.trapezoids[2]))  # !(myAccuracy = bad) AND (enemyAccuracy = bad) THEN ez
+                      min(1 - f(x, myAccuracy.trapezoids["bad"]), f(
+                          y, enemyAccuracy.trapezoids["bad"])), output.trapezoids["ez"]))  # !(myAccuracy = bad) AND (enemyAccuracy = bad) THEN ez
 
     rules.append(Rule(lambda x, y, z:
-                      min(f(z, myVisibility.trapezoids[0]), 1 - f(
-                          y, enemyAccuracy.trapezoids[2])), output.trapezoids[2]))  # (myVisibility = invisible) AND !(enemyAccuracy = pro) THEN ez
+                      min(f(z, myVisibility.trapezoids["invisible"]), 1 - f(
+                          y, enemyAccuracy.trapezoids["pro"])), output.trapezoids["ez"]))  # (myVisibility = invisible) AND !(enemyAccuracy = pro) THEN ez
 
     rules.append(Rule(lambda x, y, z:
-                      min(1 - f(z, myVisibility.trapezoids[2]), f(
-                          x, myAccuracy.trapezoids[2])), output.trapezoids[2]))  # !(myVisibility = visible) AND (myAccuracy = pro) THEN ez
+                      min(1 - f(z, myVisibility.trapezoids["visible"]), f(
+                          x, myAccuracy.trapezoids["pro"])), output.trapezoids["ez"]))  # !(myVisibility = visible) AND (myAccuracy = pro) THEN ez
 
     rules.append(Rule(lambda x, y, z:
-                      max(f(z, myVisibility.trapezoids[0]), f(
-                          x, myAccuracy.trapezoids[2])), output.trapezoids[2]))  # (myVisibility = invisible) OR (myAccuracy = pro) THEN ez
+                      max(f(z, myVisibility.trapezoids["invisible"]), f(
+                          x, myAccuracy.trapezoids["pro"])), output.trapezoids["ez"]))  # (myVisibility = invisible) OR (myAccuracy = pro) THEN ez
 
     rules.append(Rule(lambda x, y, z:
-                      f(z, myVisibility.trapezoids[2]), output.trapezoids[1]))  # (myVisibility = visible) THEN 50/50
+                      f(z, myVisibility.trapezoids["visible"]), output.trapezoids["50/50"]))  # (myVisibility = visible) THEN 50/50
 
     rules.append(Rule(lambda x, y, z:
-                      min(min(f(x, myAccuracy.trapezoids[0]), f(
-                          y, enemyAccuracy.trapezoids[2])),
-                          1 - f(z, myVisibility.trapezoids[2])), output.trapezoids[1]))  # (myAccuracy = bad) AND (enemyAccuracy = pro) AND !(myVisibility = visible) THEN 50/50
+                      min(min(f(x, myAccuracy.trapezoids["bad"]), f(
+                          y, enemyAccuracy.trapezoids["pro"])),
+                          1 - f(z, myVisibility.trapezoids["visible"])), output.trapezoids["50/50"]))  # (myAccuracy = bad) AND (enemyAccuracy = pro) AND !(myVisibility = visible) THEN 50/50
 
     rules.append(Rule(lambda x, y, z:
-                      min(1 - f(y, enemyAccuracy.trapezoids[0]), f(
-                          z, myVisibility.trapezoids[1])), output.trapezoids[1]))  # !(enemyAccuracy = bad) AND (myVisibility = slightly visible) THEN 50/50
+                      min(1 - f(y, enemyAccuracy.trapezoids["bad"]), f(
+                          z, myVisibility.trapezoids["slightly visible"])), output.trapezoids["50/50"]))  # !(enemyAccuracy = bad) AND (myVisibility = slightly visible) THEN 50/50
 
     rules.append(Rule(lambda x, y, z:
-                      min(f(x, myAccuracy.trapezoids[1]), f(
-                          y, enemyAccuracy.trapezoids[1])), output.trapezoids[1]))  # (myAccuracy = average) AND (enemyAccuracy = average) THEN 50/50
+                      min(f(x, myAccuracy.trapezoids["average"]), f(
+                          y, enemyAccuracy.trapezoids["average"])), output.trapezoids["50/50"]))  # (myAccuracy = average) AND (enemyAccuracy = average) THEN 50/50
 
     rules.append(Rule(lambda x, y, z:
-                      min(f(x, myAccuracy.trapezoids[0]), 1 - f(
-                          y, enemyAccuracy.trapezoids[0])), output.trapezoids[0]))  # (myAccuracy = bad) AND !(enemyAccuracy = bad) THEN uninstall
+                      min(f(x, myAccuracy.trapezoids["bad"]), 1 - f(
+                          y, enemyAccuracy.trapezoids["bad"])), output.trapezoids["uninstall"]))  # (myAccuracy = bad) AND !(enemyAccuracy = bad) THEN uninstall
 
     rules.append(Rule(lambda x, y, z:
-                      min(min(f(z, myAccuracy.trapezoids[2]), 1 - f(
-                          y, enemyAccuracy.trapezoids[2])),
-                          f(x, myAccuracy.trapezoids[1])), output.trapezoids[0]))  # (myVisibility = visible) AND (enemyAccuracy = pro) AND (myAccuracy = average) THEN uninstall
+                      min(min(f(z, myAccuracy.trapezoids["pro"]), 1 - f(
+                          y, enemyAccuracy.trapezoids["pro"])),
+                          f(x, myAccuracy.trapezoids["average"])), output.trapezoids["uninstall"]))  # (myVisibility = visible) AND (enemyAccuracy = pro) AND (myAccuracy = average) THEN uninstall
 
     rulesService = RulesService(rules)
 
-    for i in range(0, 5):
-        print(rulesService.get_output(
-            df.loc[i][0], df.loc[i][1], df.loc[i][2]))
+    for i in range(0, 3):
+        print(df.iloc[i])
+        out = rulesService.get_output(
+            df.loc[i][0], df.loc[i][1], df.loc[i][2])
+        X = []
+        Y = []
+        for name in output.trapezoids.keys():
+            if(out[name] > 0):
+                x = output.trapezoids[name].to_array()
+                x[1] = x[0] + (x[1]-x[0])*out[name]
+                x[2] = x[3] - (x[3]-x[2])*out[name]
+                Y_temp = [0, out[name], out[name], 0]
+                X += x
+                Y += Y_temp
+
+        sum1 = 0
+        sum2 = 0
+        XX = np.arange(X[0], X[-1], 1/1000)
+        for j in range(0, len(XX)):
+            max_f = 0
+            for t in output.trapezoids.values():
+                max_f = max(max_f, min(f(XX[j], t), out[t.name]))
+            sum1 += XX[j]*max_f
+            sum2 += max_f
+        Centroid = sum1/sum2
+        print("Centroid:", Centroid)
+        plt.plot([Centroid, Centroid], [0, 1], "r",
+                 linewidth=2, label="Centroid")
+
+        max_val = -1
+        x_sum = Centroid
+        count = 1
+
+        for j in range(0, len(Y)):
+            if Y[j] > max_val:
+                max_val = Y[j]
+                x_sum = X[j]
+                count = 1
+            elif Y[j] == max_val:
+                count += 1
+                x_sum += X[j]
+        MOM = x_sum/count
+
+        print("MOM:", MOM)
+        plt.plot([MOM, MOM], [0, 1], "y", linewidth=2, label="MOM")
+        plt.legend()
+
+        plt.fill_between(X, Y)
+        output.plot()
